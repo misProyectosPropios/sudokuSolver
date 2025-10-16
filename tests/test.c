330a78954726e9f6232fc6cccc7957c0a45354a6
misProyectosPropios <inakifrutos@outlook.com>
2025-10-16 03:16:11 +0000

    Add 2 tests to tests repetidosFila

--- tests/test.c
+++ tests/test.c
@@ -1,14 +1,53 @@
-//#include <CUnit/CUnit.h>
-//#include <CUnit/Automated.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <stdbool.h>
+#include "../src/sudoku.h"
 
- int maxi(int i1, int i2)
-    {
-      return (i1 > i2) ? i1 : i2;
-    }
+
+int maxi(int i1, int i2)
+{
+    return (i1 > i2) ? i1 : i2;
+}
 
 void test_maxi(void)
-    {
-      CU_ASSERT(maxi(0,2) == 2);
-      CU_ASSERT(maxi(0,-2) == 0);
-      CU_ASSERT(maxi(2,2) == 2);
+{
+    CU_ASSERT(maxi(0,2) == 2);
+}
+
+void test_seRepitenNumerosEnFilas_matrizVacia(void) {
+  struct sudoku vacio;    
+      
+  for(int i = 0; i < sudoku_size; i++) {
+    for(int j = 0; j < sudoku_size; j++) {
+      vacio.matriz[i][j] = sudoku_empty_cell;
     }
+  }
+  bool respuesta = seRepitenNumerosEnFilas(&vacio);
+  CU_ASSERT(!respuesta);
+}
+
+void test_seRepitenNumerosEnFilas_matrizIgual(void) {
+  struct sudoku vacio;    
+      
+  for(int i = 0; i < sudoku_size; i++) {
+    for(int j = 0; j < sudoku_size; j++) {
+      vacio.matriz[i][j] = 1;
+    }
+  }
+  bool respuesta = seRepitenNumerosEnFilas(&vacio);
+  CU_ASSERT(respuesta);
+}
+
+int main() {
+  
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("Se repiten numeros en fila?", 0, 0);
+    CU_add_test(suite, "numeros_repetidos_en_fila", test_seRepitenNumerosEnFilas_matrizVacia);
+    CU_add_test(suite, "numeros_repetidos_en_fila", test_seRepitenNumerosEnFilas_matrizIgual);
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return 0;
+}
+
